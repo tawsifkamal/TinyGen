95e429afd83b93cc331c1b1d6b0c17beba423a24
tawsif-kamal <tawsif.kamal@ibm.com>
2024-03-03 20:32:07 -0500

    changing UI code

--- frontend/pages/1_📈_Data.py
+++ frontend/pages/1_📈_Data.py
@@ -5,7 +5,7 @@ import pandas as pd
 # Set page config
 st.set_page_config(page_title="Data", page_icon="📈")
 # Add a link to the sidebar
-st.sidebar.markdown('[Go to Google](https://www.google.com)', unsafe_allow_html=True)
+st.sidebar.markdown('[Learn More About Tiny Gen](https://tawsifkamal.notion.site/Tiny-Gen-3bc759c6254a4e33ad7f7fac86d97c0b?pvs=4)', unsafe_allow_html=True)
 
 # Main page title
 st.title('📈 Tiny Gen Past Calls')
